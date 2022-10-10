cdc3844737789594d6c4d3a1707c73bbd62e3ad8
Srdjanmaric777 <61879353+Srdjanmaric777@users.noreply.github.com>
2022-10-10 20:20:38 +0200

    Null
    Null Pattern - Instead of putting if check for a null value, Null Object reflects a do nothing relationship. Such Null object can also be used to provide default behavior in case data is not available

---
+++ java patterns/src/services/NullService/AbstractCustomer.java
@@ -0,0 +1,8 @@
+package services.NullService;
+
+public abstract class AbstractCustomer {
+
+    protected String name;
+    public abstract boolean isNil();
+    public abstract String getName();
+}
---
+++ java patterns/src/services/NullService/impl/RealCustomer.java
@@ -0,0 +1,20 @@
+package services.NullService.impl;
+
+import services.NullService.AbstractCustomer;
+
+public class RealCustomer extends AbstractCustomer {
+
+    public RealCustomer(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean isNil() {
+        return false;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}

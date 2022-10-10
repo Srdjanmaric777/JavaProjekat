0fac8108388f638b3591dd3896cb42445d2472f9
Srdjanmaric777 <61879353+Srdjanmaric777@users.noreply.github.com>
2022-10-10 16:33:08 +0200

    Observer
    Observer Pattern - Observer pattern is used when there is one-to-many relationship between objects such as if one object is modified, its dependent objects are to be notified automatically

---
+++ java patterns/src/services/ObeserverService/Observer.java
@@ -0,0 +1,6 @@
+package services.ObeserverService;
+
+public abstract class Observer {
+    protected Subject subject;
+    public abstract void update();
+}
---
+++ java patterns/src/services/ObeserverService/impl/BinaryObserver.java
@@ -0,0 +1,18 @@
+package services.ObeserverService.impl;
+
+import services.ObeserverService.Observer;
+import services.ObeserverService.Subject;
+
+public class BinaryObserver extends Observer {
+
+    public BinaryObserver(Subject subject) {
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println( "Binary String: "
+                + Integer.toBinaryString(subject.getState()));
+    }
+}
